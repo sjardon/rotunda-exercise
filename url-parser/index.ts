d3b215d609787e86e5b853d6909b776d5ef95aa4
sjardon <santiagohernanjardon@gmail.com>
2023-05-23 18:23:58 -0300

    add some context for url parser example

--- url-parser/index.ts
+++ url-parser/index.ts
@@ -4,6 +4,7 @@ function run() {
   const urlParser = new UrlParser();
 
   console.log(
+    "Testing url instance [/6/api/listings/3?sort=desc&limit=10] for url schema [/:version/api/:collection/:id]",
     urlParser.parse(
       "/:version/api/:collection/:id",
       "/6/api/listings/3?sort=desc&limit=10"
